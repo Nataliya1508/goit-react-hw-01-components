d25d5b7c99a0fb208aa4db239d9aa41a52e6ffe7
Nataly <97055353+Nataliya1508@users.noreply.github.com>
2022-09-10 20:56:13 +0300

    Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import { Profile } from './Profile/Profile';
 import user from '../data/user.json';
 
+import { Statistics } from './Statistics/Statistics';
+import data from '../data/data.json';
+
 
 export const App = () => {
   const { username, tag, location, avatar, stats } = user;
@@ -15,6 +18,9 @@ export const App = () => {
           avatar={avatar}
           stats={stats}
         />
+      </>
+       <>
+        <Statistics title="Upload stats" stats={data} />
       </>
     </>
   );
